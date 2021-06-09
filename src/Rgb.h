0a6ccf5bbdef26548aa95a528345db847c66dccc
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-06-09 09:53:05 +0800

    Change DrawCircle2d_internal() to private member

--- src/Rgb.h
+++ src/Rgb.h
@@ -9,13 +9,14 @@ class Rgb
     private:
         CRGB leds[NUM_LEDS];
         uint8_t ledBrightness = 63;
+        
+        void DrawCircle2d_internal(uint8_t xc, uint8_t yc, uint8_t x, uint8_t y, CRGB color);
     public:
         void Init();
         CRGB GetColorFraction(CRGB colorIn, float fraction);
         void DrawPixel2d(int x, int y, CRGB color);
         void DrawLine(float fPos, float length, CRGB color);
         void DrawSquare2d(float fX, float fY, float length, CRGB color);
-        void DrawCircle2d_internal(uint8_t xc, uint8_t yc, uint8_t x, uint8_t y, CRGB color);
         void DrawCircle2d(uint8_t xc, uint8_t yc, uint8_t r, CRGB color);
         void Draw();
 };
