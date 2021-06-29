64dde9c64a8d5bbfdb20094670e41bbc5494d722
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-06-30 03:40:26 +0800

    Implement changeable-speed for effect StaticRainbow

--- src/Effects/StaticRainbow.h
+++ src/Effects/StaticRainbow.h
@@ -3,25 +3,31 @@
 #include "KeypadParams.h"
 #include "main.h"
 #include "Rgb.h"
+#include "RangedFloat.h"
 
 class StaticRainbow : public Effect
 {
 private:
-    uint8_t rainbowState;
+    RangedFloat* rainbowState;
 
 public:
     void Load() override
     {
-        rainbowState = 0;
+        rainbowState = new RangedFloat(255.0f);
+    }
+
+    void Unload() override
+    {
+        delete rainbowState;
     }
 
     void Update(float secondsElapsed) override
     {
-        rainbowState++;
+        (*rainbowState) += secondsElapsed * 30;
 
         for (uint8_t i = 0; i < HEIGHT; i++)
             for (uint8_t j = 0; j < WIDTH; j++)
-                rgb.Draw(4 * i + j, keypad.GetState(j, i) ? CHSV((4 * i + j) * 25 - (rainbowState * 1), 255, rgb.GetBrightness())
+                rgb.Draw(4 * i + j, keypad.GetState(j, i) ? CHSV((4 * i + j) * 25 - (rainbowState->Int() * 1), 255, rgb.GetBrightness())
                                                             : CHSV(0, 0, 0));
     }
 
