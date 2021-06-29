c3ccac512a9dfbbb93d2620ef1fb7c54ad5503f3
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-06-30 03:40:20 +0800

    Implement changeable-speed for effect Rainbow

--- src/Effects/Rainbow.h
+++ src/Effects/Rainbow.h
@@ -3,26 +3,32 @@
 #include "Effect.h"
 #include "KeypadParams.h"
 #include "main.h"
+#include "RangedFloat.h"
 
 class Rainbow : public Effect
 {
 private:
-    uint8_t rainbowState;
+    RangedFloat* rainbowState;
 
 public:
     void Load() override
     {
-        rainbowState = 0;
+        rainbowState = new RangedFloat(255.0f);
+    }
+
+    void Unload() override
+    {
+        delete rainbowState;
     }
 
     void Update(float secondsElapsed) override
     {
-        rainbowState++;
+        (*rainbowState) += secondsElapsed * 30;
 
         for (uint8_t i = 0; i < WIDTH; i++)
             rgb.Draw(4*0+i,
                 rgb.Draw(4*1+i,
-                    rgb.Draw(4*2+i, CHSV(i * 10 - (rainbowState * 1), 255, rgb.GetBrightness()) )));
+                    rgb.Draw(4*2+i, CHSV(i * 10 - (rainbowState->Int() * 1), 255, rgb.GetBrightness()) )));
     }
 
     std::string GetName() override { return "rainbow"; }
