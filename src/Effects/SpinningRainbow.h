4c3909b6a541180364639d8b8159877458c792c9
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-06-30 03:18:47 +0800

    Implement changeable-speed for effect SpinningRainbow

--- src/Effects/SpinningRainbow.h
+++ src/Effects/SpinningRainbow.h
@@ -4,6 +4,7 @@
 #include "Effect.h"
 #include "KeypadParams.h"
 #include "main.h"
+#include "RangedFloat.h"
 
 class SpinningRainbow : public Effect
 {
@@ -15,27 +16,32 @@ private:
 	    return (angle < 0) ? angle + 360 : angle;
     }
 
-    uint8_t rainbowState;
+    RangedFloat* rainbowState;
     uint8_t pointToCenterAngles[HEIGHT][WIDTH]; // Angles mapped from 0-360 to 0-255
 
 public:
     void Load() override
     {
-        rainbowState = 0;
+        rainbowState = new RangedFloat(255.0f);
         for (uint8_t y = 0; y < HEIGHT; y++)
             for (uint8_t x = 0; x < WIDTH; x++)
                 pointToCenterAngles[y][x] = map(GetPointToCenterDegrees(x + 0.5f, y + 0.5f), 0, 360, 0, 255);
     }
 
+    void Unload() override
+    {
+        delete rainbowState;
+    }
+
     void Update(float secondsElapsed) override
     {
-        rainbowState--;
+        (*rainbowState) -= secondsElapsed * 30;
         
         rgb.Clear();
 
         for (uint8_t y = 0; y < HEIGHT; y++)
             for (uint8_t x = 0; x < WIDTH; x++)
-                rgb.DrawPixel2d(x, y, CHSV(pointToCenterAngles[y][x] + rainbowState, 255, rgb.GetBrightness()));
+                rgb.DrawPixel2d(x, y, CHSV(pointToCenterAngles[y][x] + rainbowState->Int(), 255, rgb.GetBrightness()));
     }
 
     std::string GetName() override { return "spinningRainbow"; }
