92b0335ba6652593e5e67d7a7c12af4be7e88a1b
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-09-08 09:42:27 +0800

    Refactor Breathing effect

--- src/Effects/Breathing.h
+++ src/Effects/Breathing.h
@@ -9,6 +9,11 @@ private:
     uint8_t state;
     float delayElapsed;
 
+    float Map(float aMin, float aMax, float bMin, float bMax, float v)
+    {
+        return (v - aMin) / (aMax - aMin) * (bMax - bMin) + bMin;
+    }
+
 public:
     void Load() override
     {
@@ -28,7 +33,9 @@ public:
                 state = 0;
         }
 
-        rgb.Fill(CHSV(rainbowHues[state], 255, (max(127, rgb.GetBrightness()) *
-                      (delayElapsed <= 4.0f / 2 ? delayElapsed / 2 : (2.0f - (delayElapsed - 2.0f)) / 2))) );
+        if (delayElapsed <= 2.0f)
+            rgb.Fill(CHSV(rainbowHues[state], 255, Map(0.0f, 2.0f, 31.0f, max(127, rgb.GetBrightness()), delayElapsed)));
+        else
+            rgb.Fill(CHSV(rainbowHues[state], 255, Map(0.0f, 2.0f, max(127, rgb.GetBrightness()), 31.0f, delayElapsed - 2.0f)));
     }
 };
